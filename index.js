24504ff39a4a7d902e4ae30476a22c98f0c22a40
Loren Wilks <Loren.wilks@gmail.com>
2022-05-26 12:55:14 -0600

    commit tree 98cc3e5c0a14e2dec73ee734ae2260762be3b10b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]} , for the wonderful surprise gift!`);
+ 
+  }
+
+  return names;
+}
+
+writeCards(names);
